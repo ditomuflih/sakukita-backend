7806fb979f42129c65b54e0fb35c8740460d1f06
ditomuflih <110583633+ditomuflih@users.noreply.github.com>
2024-06-18 19:47:32 +0700

    Update 2024_06_15_151102_create_transactions_table.php

--- database/migrations/2024_06_15_151102_create_transactions_table.php
+++ database/migrations/2024_06_15_151102_create_transactions_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTransactionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->date('date');
+            $table->string('category');
+            $table->string('type');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('transactions');
     }
-};
+}
